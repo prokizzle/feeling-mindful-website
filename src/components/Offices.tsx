6c83f09a52e27745d74f8aa18220b9cf68030b9a
Nick Prokesch <nick@prokes.ch>
2026-01-17 23:36:44 -0600

    Update office location to Austin, TX

--- src/components/Offices.tsx
+++ src/components/Offices.tsx
@@ -32,17 +32,8 @@ export function Offices({
   return (
     <ul role="list" {...props}>
       <li>
-        <Office name="Copenhagen" invert={invert}>
-          1 Carlsberg Gate
-          <br />
-          1260, København, Denmark
-        </Office>
-      </li>
-      <li>
-        <Office name="Billund" invert={invert}>
-          24 Lego Allé
-          <br />
-          7190, Billund, Denmark
+        <Office name="Austin" invert={invert}>
+          Austin, TX
         </Office>
       </li>
     </ul>
